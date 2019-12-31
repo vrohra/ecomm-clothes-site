a898524b956c01b8cb9b14815d2f782ff43d5f1f
Vinod Rohra <vrohra@tripadvisor.com>
2019-12-31 18:56:35 -0500

    Added routing for hats shopping page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
